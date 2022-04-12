95a5860f00037568a548c8613da90f4960faf9e3
pixl-bommy <pixl-bommy@mailbox.org>
2022-04-12 21:23:58 +0200

    reduce code duplication on date.ts

--- src/date.ts
+++ src/date.ts
@@ -1,10 +1,10 @@
 export function formatDate(this: Date, template: string): string {
   const year = "" + this.getFullYear();
-  const month = ("" + (this.getMonth() + 1)).padStart(2, "0");
-  const day = ("" + this.getDate()).padStart(2, "0");
-  const hours = ("" + this.getHours()).padStart(2, "0");
-  const minutes = ("" + this.getMinutes()).padStart(2, "0");
-  const seconds = ("" + this.getSeconds()).padStart(2, "0");
+  const month = twoDigits(this.getMonth() + 1);
+  const day = twoDigits(this.getDate());
+  const hours = twoDigits(this.getHours());
+  const minutes = twoDigits(this.getMinutes());
+  const seconds = twoDigits(this.getSeconds());
 
   return template
     .replace("YYYY", year)
@@ -13,4 +13,8 @@ export function formatDate(this: Date, template: string): string {
     .replace("HH", hours)
     .replace("mm", minutes)
     .replace("ss", seconds);
+}
+
+function twoDigits(num: number): string {
+  return ("" + num).padStart(2, "0");
 }
